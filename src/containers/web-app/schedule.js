041048714e6af7dce6ba875320b3b055756b2341
adrienfloor <adriaandrum@gmail.com>
2022-04-21 10:40:23 +0200

    test webview schedule

--- src/containers/web-app/schedule.js
+++ src/containers/web-app/schedule.js
@@ -506,7 +506,7 @@ class Schedule extends React.Component {
 					onSubmit={e => this.handleCreateCoaching(e)}
 					className='scroll-div-vertical card upload-form schedule'
 				>
-					<div className='medium-separator'></div>
+					{ !isWebview && <div className='medium-separator'></div> }
 					<div className='small-separator'></div>
 					<span className='small-title citrusBlack small-responsive-title-settings'>
 						{capitalize(t('newTraining'))}
