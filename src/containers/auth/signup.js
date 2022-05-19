d71de3ada3e21f16a3f23f5831d63f63de8ba9fd
adrienfloor <adriaandrum@gmail.com>
2022-05-19 10:43:36 +0200

    tryout password closed eye input

--- src/containers/auth/signup.js
+++ src/containers/auth/signup.js
@@ -244,7 +244,7 @@ class Signup extends React.Component {
 							className='text-input small-text citrusBlack input password-input'
 							type={showPassword ? 'text' : 'password'}
 							onChange={e => this.onTextInputChange(e, 'password')}
-							style={showPassword ? { height: '50px'} : { height: '52px' } }
+							style={showPassword ? { height: '45px'} : { height: '52px' } }
 						/>
 						<div
 							className='password-eye hover'
